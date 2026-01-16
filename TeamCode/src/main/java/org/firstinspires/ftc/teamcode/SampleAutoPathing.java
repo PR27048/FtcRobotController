d4ae42314b18dc09867663bbf9a42f953ecf9a20
apkashn2 <apkashn2@gmail.com>
2026-01-16 14:24:24 -0600

    added code pedro path

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutoPathing.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.pedropathing.util.Timer;
+@TeleOp
+public class SampleAutoPathing extends OpMode {
+
+    //private Follower follower;
+  //  private Timer pathTimer, opTimer;
+    public void init() {
+
+    }
+
+    public void loop(){
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -4,16 +4,73 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(5.1) //this has to be in killogram
+            .forwardZeroPowerAcceleration(-26.999) //get this by running pedropath test (i have picked a number for now)
+            .lateralZeroPowerAcceleration(-92.2222)
+            //.translationalPIDFCoefficiants(new PIDFCoefficients(0.06,0,0.0001,0.025)) //set using testing
+            //.headingPIDFCoefficiants(new PIDFCoefficients(0.71,0,0.0002,0.025)) // set by testing
+           // .centripetalScaling(0.0005) //set by testing
+            // .drivePIDFCoefficiants(new FilteredPIDFCoefficients(0.06,0,0.0001,0.025)) //set by testing
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+            ;
+//    public static MecanumConstants driveConstants = new MecanumConstants()
+//            .maxPower(1)
+//            .rightFrontMotorName("front_right")
+//            .rightRearMotorName("back_right")
+//            .leftRearMotorName("back_left")
+//            .leftFrontMotorName("front_left")
+//            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+//            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+//            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+//            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+//            .xVelocity(67.001) //get this by running pedropath test (i have picked a number for now)
+//            .yVelocity(24.55);
+
+    //goto https://pedropathing.com/docs/pathing/tuning/localization/drive-encoder to see details
+    //In the tuning OpMode, under localization, select and start the forward tuner. Then, push the robot forward 48 inches (exactly 2 field tiles). This distance is configurable if needed. Once you push the robot forward, two numbers will be displayed on telemetry:
+    //The distance the robot thinks it has traveled
+    //The multiplier; this is the number you want.
+    public double fwdMultiplier = 1; //defaulting to 1
+
+    //The lateral tuner is very similar to the forward tuner, except it is sideways. In the tuning OpMode, under localization, select and start the lateral tuner. Push the robot left 48 inches (exactly 2 field tiles). As with the forward tuner, this distance is configurable.
+    //Lastly, add the multiplier to DriveEncoderConstants by adding the following line.
+    public double latMultiplier = 1; //defaulting to 1
+
+    //The turn tuner is again, similar to both the forward tuner and lateral tuner, except it is rotational. Place the robot so it aligns to a fixed reference point (eg. edge of a field tile). In the tuning OpMode, under localization, select and start the turn tuner. Rotate the robot counterclockwise one full rotation. As with the previous tuners, this amount is configurable.
+    //Lastly, add the multiplier to DriveEncoderConstants by adding the following line.
+    public double turnMultiplier = 1; //defaulting to 1
+
+//    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
+//            .robotWidth(18) //Width: the distance between the left and right wheels
+//            .robotLength(18) //Length: the distance between the front and back wheels
+//            .forwardTicksToInches(fwdMultiplier)
+//            .strafeTicksToInches(latMultiplier)
+//            .turnTicksToInches(turnMultiplier)
+//            .rightFrontMotorName("front_right")
+//            .rightRearMotorName("back_right")
+//            .leftRearMotorName("back_left")
+//            .leftFrontMotorName("front_left")
+//            .leftFrontEncoderDirection(Encoder.REVERSE)
+//            .leftRearEncoderDirection(Encoder.REVERSE)
+//            .rightFrontEncoderDirection(Encoder.FORWARD)
+//            .rightRearEncoderDirection(Encoder.FORWARD);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99,
+            100,
+            1,
+            1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+               // .driveEncoderLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
+             //   .mechanumDrivetrain(driveConstants)
+
                 .build();
     }
 }
