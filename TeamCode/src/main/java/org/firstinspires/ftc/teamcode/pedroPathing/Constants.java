a9b42055535b050b69ddafdc7a9bbf0e92808872
apkashn2 <apkashn2@gmail.com>
2026-01-16 18:31:31 -0600

    added code pedro path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -7,6 +7,9 @@ import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+//import org.firstinspires.ftc.teamcode.localization.constants;
+import org.firstinspires.ftc.teamcode.drivetrains.*;
+import org.firstinspires.ftc.teamcode.localization.constants.DriveEncoderConstants;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
@@ -19,18 +22,18 @@ public class Constants {
             // .drivePIDFCoefficiants(new FilteredPIDFCoefficients(0.06,0,0.0001,0.025)) //set by testing
 
             ;
-//    public static MecanumConstants driveConstants = new MecanumConstants()
-//            .maxPower(1)
-//            .rightFrontMotorName("front_right")
-//            .rightRearMotorName("back_right")
-//            .leftRearMotorName("back_left")
-//            .leftFrontMotorName("front_left")
-//            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-//            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-//            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-//            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-//            .xVelocity(67.001) //get this by running pedropath test (i have picked a number for now)
-//            .yVelocity(24.55);
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("front_right")
+            .rightRearMotorName("back_right")
+            .leftRearMotorName("back_left")
+            .leftFrontMotorName("front_left")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(67.001) //get this by running pedropath test (i have picked a number for now)
+            .yVelocity(24.55);
 
     //goto https://pedropathing.com/docs/pathing/tuning/localization/drive-encoder to see details
     //In the tuning OpMode, under localization, select and start the forward tuner. Then, push the robot forward 48 inches (exactly 2 field tiles). This distance is configurable if needed. Once you push the robot forward, two numbers will be displayed on telemetry:
@@ -46,20 +49,20 @@ public class Constants {
     //Lastly, add the multiplier to DriveEncoderConstants by adding the following line.
     public double turnMultiplier = 1; //defaulting to 1
 
-//    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
-//            .robotWidth(18) //Width: the distance between the left and right wheels
-//            .robotLength(18) //Length: the distance between the front and back wheels
-//            .forwardTicksToInches(fwdMultiplier)
-//            .strafeTicksToInches(latMultiplier)
-//            .turnTicksToInches(turnMultiplier)
-//            .rightFrontMotorName("front_right")
-//            .rightRearMotorName("back_right")
-//            .leftRearMotorName("back_left")
-//            .leftFrontMotorName("front_left")
-//            .leftFrontEncoderDirection(Encoder.REVERSE)
-//            .leftRearEncoderDirection(Encoder.REVERSE)
-//            .rightFrontEncoderDirection(Encoder.FORWARD)
-//            .rightRearEncoderDirection(Encoder.FORWARD);
+    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
+            .robotWidth(18) //Width: the distance between the left and right wheels
+            .robotLength(18) //Length: the distance between the front and back wheels
+            .forwardTicksToInches(fwdMultiplier)
+            .strafeTicksToInches(latMultiplier)
+            .turnTicksToInches(turnMultiplier)
+            .rightFrontMotorName("front_right")
+            .rightRearMotorName("back_right")
+            .leftRearMotorName("back_left")
+            .leftFrontMotorName("front_left")
+            .leftFrontEncoderDirection(Encoder.REVERSE)
+            .leftRearEncoderDirection(Encoder.REVERSE)
+            .rightFrontEncoderDirection(Encoder.FORWARD)
+            .rightRearEncoderDirection(Encoder.FORWARD);
     public static PathConstraints pathConstraints = new PathConstraints(0.99,
             100,
             1,
