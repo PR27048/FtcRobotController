8870470b47130dd719cf19135d4939c5b655d700
nishanthk12 <nishanthkarupakula@gmail.com>
2026-01-19 10:28:27 -0600

    state bot code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/ServiceHelperNishanth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/ServiceHelperNishanth.java
@@ -23,6 +23,7 @@ public class ServiceHelperNishanth {
         ServoConFront = hwMap.get(CRServo.class, "servo_con_front_transfer");
         ServoConIntake = hwMap.get(CRServo.class, "intakeservo");
         ServoConTurret = hwMap.get(CRServo.class, "servo_con_turret");
+        ServoConHood = hwMap.get(CRServo.class, "servo_con_hood");
 
         FrontRight.setDirection(DcMotor.Direction.REVERSE);
         BackRight.setDirection(DcMotor.Direction.REVERSE);
