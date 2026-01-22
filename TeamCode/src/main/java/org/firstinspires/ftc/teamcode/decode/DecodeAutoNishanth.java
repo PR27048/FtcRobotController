1375dc3604c885fc89cffb1cf23e11280c0981a4
nishanthk12 <nishanthkarupakula@gmail.com>
2026-01-21 21:20:43 -0600

    auto code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/DecodeAutoNishanth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/DecodeAutoNishanth.java
@@ -10,26 +10,31 @@ public class DecodeAutoNishanth extends OpMode {
 
     ServiceHelperNishanth serviceHelper = new ServiceHelperNishanth();
 
-    double IntakePower = 0.8;
+   // double IntakePower = 0.8;
     double TurretPower = 0.55;
 
     @Override
     public void init() {
+
         serviceHelper.init(hardwareMap);
     }
 
     @Override
     public void start() {
      //   serviceHelper.SetIntakePower(IntakePower);
+        serviceHelper.SetServoConIntakePower(-0.8);
         serviceHelper.SetTurretPower(TurretPower);
         serviceHelper.SetServoConFrontPower(-0.8);
-        serviceHelper.drive(0.5, 0.0, 0.0);
+        serviceHelper.SetServoConBackPower(-0.8);
+        serviceHelper.drive(0.75, 0.0, 0.0);
         try { Thread.sleep(2000); } catch (Exception e) {}
         serviceHelper.drive(0.0, 0.0, 0.0);
         serviceHelper.SetTurretPower(TurretPower);
 
         serviceHelper.SetServoConBackPower(0.8);
         try { Thread.sleep(500); } catch (Exception e) {}
+        serviceHelper.SetServoConFrontPower(0.0);
+        serviceHelper.SetServoConBackPower(0.0);
     }
 
     @Override
@@ -39,6 +44,7 @@ public class DecodeAutoNishanth extends OpMode {
 
     @Override
     public void stop() {
+
         serviceHelper.drive(0.0, 0.0, 0.0);
     }
 }
