cace592ec0bbd9a2c2f55d8ef8f9b680f0c83c9a
nishanthk12 <nishanthkarupakula@gmail.com>
2026-01-25 12:44:47 -0600

    auto code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/DecodeAutoNishanth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/DecodeAutoNishanth.java
@@ -11,7 +11,7 @@ public class DecodeAutoNishanth extends OpMode {
     ServiceHelperNishanth serviceHelper = new ServiceHelperNishanth();
 
    // double IntakePower = 0.8;
-    double TurretPower = 0.9;
+    double TurretPower = 0.55;
 
     @Override
     public void init() {
