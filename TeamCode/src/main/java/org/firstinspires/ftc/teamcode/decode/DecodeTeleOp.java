0ddcfd73938b620ac0c03953c0c4070352dee543
apkashn2 <apkashn2@gmail.com>
2026-01-25 13:10:37 -0600

    1/25/26 - Teleop final version

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/DecodeTeleOp.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.decode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+@TeleOp
+public class DecodeTeleOp extends OpMode{
+    ServiceHelper serviceHelper = new ServiceHelper();
+    double forward, strafe, rotate;
+
+    @Override
+    public void init() {
+        serviceHelper.init(hardwareMap);
+
+    }
+
+    @Override
+    public void start() {
+        serviceHelper.SetTurretPower(0.47);
+    }
+
+    @Override
+    public void loop() {
+        forward = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+        serviceHelper.drive(forward, strafe, rotate);
+
+        if (gamepad1.right_trigger > 0.1) { // shoot
+             serviceHelper.SetIntakePower(1.0);
+             serviceHelper.SetServoConFrontPower(-1.0);
+             serviceHelper.setFeederPower(-0.7);
+             serviceHelper.SetServoConFrontPower(-1.0);
+             serviceHelper.setIntakeServoPower(-1.0);
+             serviceHelper.SetTurretPower(0.52);
+
+         }
+         else {
+             stop();
+         }
+
+        if (gamepad2.right_trigger > 0.1) {  //intake + store
+            serviceHelper.SetIntakePower(1.0);
+            serviceHelper.SetServoConFrontPower(-1.0);
+            serviceHelper.setFeederPower(0.7);
+            serviceHelper.SetServoConFrontPower(-1.0);
+            serviceHelper.setIntakeServoPower(-1.0);
+            serviceHelper.SetTurretPower(0.47);
+        }
+
+        if (gamepad2.dpad_up) {
+            serviceHelper.setHoodAngle(0.15);
+        }
+        if (gamepad2.dpad_down) {
+            serviceHelper.setHoodAngle(-0.15);
+        }
+        double rightStick = gamepad2.right_stick_x;
+        double clockwise = 0;
+        double counterclockwise = 0;
+
+        if (rightStick > 0.05) {
+            clockwise -= rightStick;
+        }
+        if (rightStick < -0.05) {
+            counterclockwise = rightStick;
+        }
+
+        serviceHelper.aimTurret(clockwise, counterclockwise);
+    }
+    @Override
+    public void stop() {
+        serviceHelper.SetIntakePower(0.0);
+        serviceHelper.SetServoConFrontPower(0.0);
+        serviceHelper.setFeederPower(0.0);
+        serviceHelper.SetServoConFrontPower(0.0);
+        serviceHelper.setIntakeServoPower(0.0);
+        serviceHelper.SetTurretPower(0.0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/ServiceHelper.java
@@ -0,0 +1,113 @@
+package org.firstinspires.ftc.teamcode.decode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class ServiceHelper {
+    private DcMotor FrontLeft, FrontRight, BackLeft, BackRight, Intake, Turret, MotorFeeder;
+    private CRServo ServoConFront, IntakeServo, ServoConTurret;
+    private Servo HoodServo;
+    private ElapsedTime driveTimer = new ElapsedTime();
+
+    public void init(HardwareMap hwMap) {
+        FrontLeft =hwMap.get(DcMotor .class,"front_left");
+        FrontRight =hwMap.get(DcMotor .class,"front_right");
+        BackLeft =hwMap.get(DcMotor .class,"back_left");
+        BackRight =hwMap.get(DcMotor .class,"back_right");
+
+        //intake and intake feeder and flywheel
+        Intake =hwMap.get(DcMotor .class,"intake");
+        MotorFeeder =hwMap.get(DcMotor .class,"motorizedtransfer");
+        Turret =hwMap.get(DcMotor .class,"turret");
+
+        //initialize servos
+        IntakeServo =hwMap.get(CRServo .class,"intakeservo");
+        ServoConFront =hwMap.get(CRServo .class,"servo_con_front_transfer");
+
+        ServoConTurret =hwMap.get(CRServo .class,"servo_con_turret");
+        HoodServo =hwMap.get(Servo .class,"hoodservo");
+
+        //reverse wheel position
+        FrontRight.setDirection(DcMotor.Direction.REVERSE);
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
+
+        FrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        MotorFeeder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        Intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        MotorFeeder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+
+    public void drive(double forward, double strafe, double rotate) {
+        double FrontLeftPower = forward - strafe - rotate;
+        double FrontRightPower = forward + strafe + rotate;
+        double BackLeftPower = - forward - strafe + rotate;
+        double BackRightPower = forward - strafe + rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;
+
+        maxPower = Math.max(maxPower, Math.abs(FrontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(FrontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(BackLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(BackRightPower));
+
+        FrontLeft.setPower(maxSpeed * FrontLeftPower / maxPower);
+        FrontRight.setPower(maxSpeed * FrontRightPower / maxPower);
+        BackLeft.setPower(maxSpeed * BackLeftPower / maxPower);
+        BackRight.setPower(maxSpeed * BackRightPower / maxPower);
+    }
+
+    //intake motor
+    public void SetIntakePower(double IntakePower) {
+
+        Intake.setPower(IntakePower);
+    }
+    //intake servo power
+    public void setIntakeServoPower(double power) {
+        IntakeServo.setPower(power);
+    }
+
+    public void SetServoConFrontPower(double frontPower) {
+        ServoConFront.setPower(frontPower);
+    }
+    public void SetTurretPower(double TurretPower) {
+
+        Turret.setPower(TurretPower);
+        // Turret.setPower(0.47); //0.64
+    }
+    public void setFeederPower(double feederPower) {
+        MotorFeeder.setPower(feederPower);
+    }
+
+    public void setHoodAngle(double angle) {
+        HoodServo.setPosition(angle);
+    }
+    public void aimTurret(double clockwise, double counterclockwise) {
+        double ServoConTurretPower = clockwise - counterclockwise;
+
+        double MaxTurretAimingPower = 1.0;
+        double MaxTurretAimingSpeed = 1.0;
+
+        MaxTurretAimingPower = Math.max(MaxTurretAimingPower, Math.abs(ServoConTurretPower));
+
+        ServoConTurret.setPower((MaxTurretAimingSpeed * ServoConTurretPower / MaxTurretAimingPower));
+    }
+
+}
