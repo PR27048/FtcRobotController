af86073a5cb94fd6380062dd46e5d66f6cb44677
nishanthk12 <nishanthkarupakula@gmail.com>
2026-01-27 20:39:56 -0600

    auto code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/DecodeTeleOPAaditya.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/DecodeTeleOPAaditya.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-@Disabled
+
 @TeleOp
 public class DecodeTeleOPAaditya extends OpMode {
 
