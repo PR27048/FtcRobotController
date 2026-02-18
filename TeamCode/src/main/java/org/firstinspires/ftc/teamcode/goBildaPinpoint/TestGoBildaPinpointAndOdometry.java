01ebcd5e1bba2fb2c206be508cfccf7f4d8e3ec7
nexfind <nexfind@gmail.com>
2026-02-17 20:23:01 -0600

    add goBilda base  code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/goBildaPinpoint/TestGoBildaPinpointAndOdometry.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.goBildaPinpoint;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
+
+
+import java.util.Locale;
+
+
+public class TestGoBildaPinpointAndOdometry extends LinearOpMode{
+
+    GoBildaPinpointDriver odo;
+
+
+    @Override
+    public void runOpMode() {
+
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+
+        odo.setOffsets(-88.9, -190.5, DistanceUnit.MM);
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD,
+                GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        odo.resetPosAndIMU();
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("X offset", odo.getXOffset(DistanceUnit.MM));
+        telemetry.addData("Y offset", odo.getYOffset(DistanceUnit.MM));
+        telemetry.addData("Device Version Number:", odo.getDeviceVersion());
+        telemetry.addData("Heading Scalar", odo.getYawScalar());
+        telemetry.update();
+
+
+        waitForStart();
+        resetRuntime();
+
+        while (opModeIsActive()) {
+
+            odo.update();
+
+            if (gamepad1.a){
+                odo.resetPosAndIMU(); //resets the position to 0 and recalibrates the IMU
+            }
+
+            if (gamepad1.b){
+                odo.recalibrateIMU(); //recalibrates the IMU without resetting position
+            }
+
+
+
+            Pose2D pos = odo.getPosition();
+            String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}",
+                    pos.getX(DistanceUnit.MM),
+                    pos.getY(DistanceUnit.MM),
+                    pos.getHeading(AngleUnit.DEGREES));
+            telemetry.addData("Position", data);
+
+            telemetry.addData("Status", odo.getDeviceStatus());
+
+            telemetry.update();
+
+        }
+
+    }
+}
