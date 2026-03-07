487d7ecd7982e5a91736b0d796e0bd0622463652
nishanthk12 <nishanthkarupakula@gmail.com>
2026-03-06 19:58:30 -0600

    pedropathing near

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto_Red_Near_With_PedroPathing_Full.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto_Red_Near_With_PedroPathing_Full.java
@@ -388,10 +388,12 @@ public class Auto_Red_Near_With_PedroPathing_Full extends OpMode {
             case SHOOT_3:
             case SHOOT_4:
                 MotorFeeder.setPower(-1.0);
+                ServoConFront.setPower(-1.0);
                 break;
 
             default:
                 MotorFeeder.setPower(1.0);
+                ServoConFront.setPower(0.6);
                 break;
         }
 
